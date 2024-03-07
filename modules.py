7fb578b8aefad90909826988b3716a9722f9a666
nat <dkl.gmail.com>
2024-03-07 10:47:42 +0100

    Add modules

---
+++ modules.py
@@ -0,0 +1,11 @@
+def dodawanie(x,y):
+    return x+y
+def odejmowanie(x,y):
+    return x-y
+def mnozenie(x,y):
+    return x*y
+def dzielenie(x,y):
+    if(y!=0):
+       return x/y
+    else:
+        print("Nie mozna dzielic przez 0.")
